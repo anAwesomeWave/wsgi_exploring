9a6775025bde93573f3b5f2a7e057db28a4a7984
Tim Zubov <timofey.zubov.05@gmail.com>
2023-06-27 22:41:07 +0300

    Add logging middleware to wsgi server.

--- simple_wsgi_server.py
+++ simple_wsgi_server.py
@@ -1,9 +1,9 @@
 from wsgiref.simple_server import make_server
 from urllib.parse import parse_qs
 import argparse
+import logging
 
 '''
-TODO:   app itself, auth middleware, logger middleware, args
 https://www.fullstackpython.com/wsgi-servers.html
 '''
 
@@ -14,6 +14,8 @@ def simple_app(environ, start_response):
     response = status
     if user:
         response = f'Hy {user["user"]}!'
+
+    logging_middleware(environ, user)
     start_response(status, headers)
     return [response.encode()]
 
@@ -26,10 +28,29 @@ def auth_middleware(environ):
     return [], '401 Unauthorized'
 
 
+def logging_middleware(environ, user):
+    if not user:
+        msg = f'''
+        Someone tried to access website...
+        ---DATA ABOUT HIM---
+        HTTP_USER_AGENT: {environ.get("HTTP_USER_AGENT")}
+        HTTP_REFERER: {environ.get("HTTP_REFERER")}
+        HTTP_SEC_CH_UA: {environ.get("HTTP_SEC_CH_UA")}
+        --------------------'''
+        logging.warning(msg)
+    else:
+        msg = f"User '{user['user']}' access website."
+        logging.info(msg)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Specify some parameters.')
     parser.add_argument('-p', '--port', type=int, default=8000, required=False, help='Specify server port.')
     d = vars(parser.parse_args())
+
+    logging.basicConfig(level=logging.INFO, filename='wsgi_log.log', format="%(asctime)s %(levelname)s %(message)s")
+
     with make_server('', d['port'], simple_app) as httpd:
+        logging.info(f'Start serving at port {d["port"]}')
         print(f'Serving at port {d["port"]}...')
         httpd.serve_forever()

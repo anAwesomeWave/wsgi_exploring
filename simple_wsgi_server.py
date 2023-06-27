4801e33c39a8138488b3c058770af0eb2823fb68
Tim Zubov <timofey.zubov.05@gmail.com>
2023-06-27 21:15:36 +0300

    Add authentication middleware function.

--- simple_wsgi_server.py
+++ simple_wsgi_server.py
@@ -1,18 +1,30 @@
 from wsgiref.simple_server import make_server
+from urllib.parse import parse_qs
 
 PORT = 8000
 
 '''
 TODO:   app itself, auth middleware, logger middleware, args
-
+https://www.fullstackpython.com/wsgi-servers.html
 '''
 
 
 def simple_app(environ, start_response):
-    status = '200 OK'
+    user, status = auth_middleware(environ)
     headers = [('Content-type', 'text/plain; charset=utf-8')]
+    response = status
+    if user:
+        response = f'Hy {user["user"]}!'
     start_response(status, headers)
-    return [b'Hello from wsgi-app.']
+    return [response.encode()]
+
+
+def auth_middleware(environ):
+    query = parse_qs(environ['QUERY_STRING'])
+    user = query.get('user')
+    if user:
+        return {'user': user[0]}, '200 OK'
+    return [], '401 Unauthorized'
 
 
 with make_server('', PORT, simple_app) as httpd:
